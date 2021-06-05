cb9a28d0084d178d8a11d188057393e1fe7d3779
tannerlsmith <tanner.smith@uvm.edu>
2021-06-04 21:08:56 -0400

    added divs within button to hold weather info.

---
+++ script.js
@@ -0,0 +1,12 @@
+// function to generate display in 7 day boxes
+
+    // var to hold the selected weather info using id-day1,day2 etc
+
+    // fetch API for daily weather info 
+
+    // add the date parameter
+
+    // then function display
+
+
+
