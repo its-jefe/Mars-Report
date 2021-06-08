492c9ef7a897138271c80a7742a2854956ec8b66
Jeff Jobs <Jeff.Timson@Yahoo.com>
2021-06-07 21:18:30 -0400

    logged all the api's we will need to use. still not sure if insight api can access more dates than seeing now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,45 @@
+// mars rover photos from the 3 different rovers
 let curiosity = "https://api.nasa.gov/mars-photos/api/v1/manifests/Curiosity/?api_key=RZGwSzKbOaZadLPNj2btegTPGivRWJI8IKnLnUsd"
-
 let spirit = "https://api.nasa.gov/mars-photos/api/v1/manifests/spirit/?api_key=RZGwSzKbOaZadLPNj2btegTPGivRWJI8IKnLnUsd"
-
 let opportunity = "https://api.nasa.gov/mars-photos/api/v1/manifests/opportunity/?api_key=RZGwSzKbOaZadLPNj2btegTPGivRWJI8IKnLnUsd"
 
+// insight weather 
+let insight = "https://api.nasa.gov/insight_weather/?api_key=" + "RZGwSzKbOaZadLPNj2btegTPGivRWJI8IKnLnUsd" + "&feedtype=json&ver=1.0"
+
 fetch(curiosity).then(function (response) {
+    if (response.ok) {
+      response.json()
+        .then(function (data) {
+          console.log(data)
+        });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  });
+
+  fetch(spirit).then(function (response) {
+    if (response.ok) {
+      response.json()
+        .then(function (data) {
+          console.log(data)
+        });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  });
+
+  fetch(opportunity).then(function (response) {
+    if (response.ok) {
+      response.json()
+        .then(function (data) {
+          console.log(data)
+        });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  });
+
+  fetch(insight).then(function (response) {
     if (response.ok) {
       response.json()
         .then(function (data) {
