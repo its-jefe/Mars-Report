fa22f166f6bfa49382d9d96b0b6bb7c833f75070
Jeff Jobs <Jeff.Timson@Yahoo.com>
2021-06-09 10:40:45 -0400

    updating branch with our work from last night

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,10 @@ let opportunity = "https://api.nasa.gov/mars-photos/api/v1/manifests/opportunity
 // Mars Science Laboratory
 let weather = "https://mars.nasa.gov/rss/api/?feed=weather&category=msl&feedtype=json"
 
+let weatherBtnEl = document.getElementById("weather-container")
+weatherBtnEl.addEventListener("click", solClick);
+
+// on button click function - passes name of rover clicked
 function sendName(something) {
   let apiCall = "https://api.nasa.gov/mars-photos/api/v1/manifests/" + something + "/?api_key=RZGwSzKbOaZadLPNj2btegTPGivRWJI8IKnLnUsd"
   let holder
@@ -26,13 +30,37 @@ function sendName(something) {
 
 function newFunction(holder) {
   console.log(holder);
-}
+  photos = holder.photo_manifest.photos
+  // find the date!
+  for (var i = photos.length-1; i >= 0; i --){
+    if (photos[i].earth_date == "2021-06-02"){
+      console.log("yes!");
+    };
+  };
+};
 
+function solClick(event) {
+  // get the language attribute from the clicked element
+
+  let whichSol = event.target.parentElement.parentElement.getAttribute("data-date");
+
+  console.log(whichSol)
+
+  alert(whichSol);
+
+  // if (whichSol) {
+  //   getFeaturedRepos(language);
+
+  //   // clear old content
+  //   repoContainerEl.textContent = "";
+  // }
+};
+
+// get the weather!
 fetch(weather).then(function (response) {
   if (response.ok) {
     response.json()
       .then(function (data) {
-        console.log("save")
         console.log(data)
         for (var i = 4; i >= 0; i--) {
           let newDivEl = document.createElement("div");
@@ -45,16 +73,21 @@ fetch(weather).then(function (response) {
           let high = "High: " + (data.soles[i].max_temp);
           let low = "Low: " + (data.soles[i].min_temp);
           let atmosphere = "'./assets/images/" + (data.soles[i].atmo_opacity) + ".png'"; //sunny - cloudy - windy
-          console.log(atmosphere)
 
           dateDivEl.innerHTML = "<div>" + date + "</div>" + "<div style='font-weight: 300'>" + sol + "</div>";
+          dateDivEl.style.userSelect = "none";
           photoDivEl.innerHTML = "<img src=" + atmosphere + " width='100' height='100' style='margin:auto'>";
+          photoDivEl.style.userSelect = "none";
           tempDivEl.innerHTML = "<div style='color:red'>" + high + "</div>" + "<div style='color:blue'>" + low + "</div>";
+          tempDivEl.style.userSelect = "none";
+
           newDivEl.appendChild(dateDivEl);
           newDivEl.appendChild(photoDivEl);
           newDivEl.appendChild(tempDivEl);
+          newDivEl.style.userSelect = "none";
           newDivEl.classList.add('items-stretch');
           document.getElementById("sol" + (i + 1)).appendChild(newDivEl);
+          document.getElementById("sol" + (i + 1)).setAttribute('data-date', date);;
         }
       });
   } else {
@@ -62,3 +95,4 @@ fetch(weather).then(function (response) {
   }
 
 });
+
